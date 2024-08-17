52c1a50178fdcb7a35b91d6f454617a0b736715b
Bocdagla <tomeu1m1a1@gmail.com>
2024-08-17 17:56:55 +0200

    added level selection

--- src/scripts/gameplay/state/victory_spot.gd
+++ src/scripts/gameplay/state/victory_spot.gd
@@ -1,4 +1,8 @@
 class_name VictorySpot;
 extends Area3D
 
+@export var next_level_index: int;
+@export var following_level_index: int;
+
 func _on_body_entered(body: Node3D) -> void:
+	GameManager.next_level(next_level_index, following_level_index);
