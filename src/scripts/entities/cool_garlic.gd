56bbc46a06f68733fbd0ed4f5e408669ded51c91
Nanocirs <nanocirs@gmail.com>
2024-08-18 00:41:05 +0200

    Ajo molón is more molón now

--- src/scripts/entities/cool_garlic.gd
+++ src/scripts/entities/cool_garlic.gd
@@ -1,1 +1,9 @@
 extends Node
+
+@export var player: Node3D = null;
+
+@onready var garlic_head: Node3D = $Body;
+
+
+func _process(delta: float) -> void:
+	garlic_head.look_at(player.global_position, Vector3.UP, true);
