7e974a88008f20eada0e5c2d1ce2fe52fee17377
Bocdagla <tomeu1m1a1@gmail.com>
2024-08-18 02:37:44 +0200

    Added first 3 levels

--- src/scripts/board/board_item.gd
+++ src/scripts/board/board_item.gd
@@ -70,8 +70,8 @@ func setMaterial():
 		meshInstance.material_override = BoardItemResource.getMaterial(color, BoardItemResource.MaterialType.HOVER);
 	else:
 		meshInstance.material_override = BoardItemResource.getMaterial(color, BoardItemResource.MaterialType.NORMAL);
-	
-		
+
+
 
 func connectPickupJustOnce(call: Callable):
 	if _lastConnection != null:
