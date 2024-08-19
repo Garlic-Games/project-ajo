7fe8018670a123df9c1240585bf63a05205c95cf
Soteck <tukav73@gmail.com>
2024-08-19 23:48:34 +0200

    Add credits, error feedback on load

--- src/scripts/main_menu/main_menu.gd
+++ src/scripts/main_menu/main_menu.gd
@@ -4,7 +4,7 @@ extends CanvasLayer;
 @onready var credits: Credits = %Credits;
 @onready var loading_panel: LoadingPanel = %LoadingPanel;
 @export_file("*.tscn") var gameplay_scene: String;
-
+@onready var continueGameButton : BaseButton = $MainContainer/MainMargin/Buttons/ContinueGame;
 func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE;
 	assert(gameplay_scene != "", "A gameplay scene resource must be provided to main menu")
@@ -15,6 +15,8 @@ func start_game() -> void:
 func continue_game() -> void:
 	if GameManager.load_game():
 		SceneLoader.load_scene(gameplay_scene);
+	else:
+		continueGameButton.text = "Error loading game"
 
 func open_settings() -> void:
 	settings_menu.open();
