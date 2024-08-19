57ea4be9c78e2eb01bc68a35da139943249d3044
Soteck <tukav73@gmail.com>
2024-08-19 18:02:00 +0200

    New main menu background map

--- src/scripts/scenes/menu_loop_scene.gd
+++ src/scripts/scenes/menu_loop_scene.gd
@@ -1,13 +1,16 @@
 extends Node3D
 
-@export var pathPoints: Array[Node3D] =[];
+@export var pathPointsParent: Node3D;
 @onready var camera: Camera3D = $Camera3D;
 
+var _pathPoints: Array[Node3D] = [];
 func _ready() -> void:
-	camera.global_transform = pathPoints[pathPoints.size()-1].global_transform;
+	_pathPoints.append_array(pathPointsParent.get_children());
+	camera.global_transform = _pathPoints[_pathPoints.size()-1].global_transform;
 	var tween = get_tree().create_tween();
-	for point in pathPoints:
-		tween.tween_property(camera, "global_position", point.global_position, 5);
-		tween.tween_property(camera, "global_rotation", point.global_rotation, 0.5);
+	for point in _pathPoints:
+		#tween.tween_property(camera, "global_position", point.global_position, 2);
+		#tween.tween_property(camera, "global_rotation", point.global_rotation, 0.5);
+		tween.tween_property(camera, "global_transform", point.global_transform, 3);
 		point.visible = false;
 	tween.set_loops(60);
